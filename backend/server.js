2596fa18693511c490de3ee001f3f075718ddba7
Tomás Altamirano <tomas.altamirano@anahuac.mx>
2025-10-17 20:24:26 -0500

    Tu mensaje descriptivo

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('hola mundo')
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Servidor Iniciado en el puerto ${port}`))
+
+app.get('/api/saludo', (req, res) => {
+    res.send('Hola Mundo');
+})
