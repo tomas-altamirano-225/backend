820d01b6ca3c7bdc61e0ee432dbfe04424809ccd
Tomás Altamirano <tomas.altamirano@anahuac.mx>
2025-11-25 15:09:24 -0500

    Ultimo commit...

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const  cors = require('cors');
 
 const port = process.env.PORT || 5001;
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
